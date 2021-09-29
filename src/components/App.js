4c683d014bf241e88e8bf97cfc2369a1c369fcf3
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-28 23:25:00 -0500

    commit tree cc145674da12243a0d25fc62c19c5df73d2d1e1b

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React, {useState, useEffect} from 'react';
+
+function App() {
+  const [dogImageURL, setDogImageURL] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+      setDogImageURL(data.message);
+      setIsLoaded(true);
+    });
+  }, []);
+
+  // if data hasn't been loaded, show a loading indicator
+  if (!isLoaded) {
+    return <h3>Loading...</h3>;
+  }
+
+  return (
+    <>
+      <img src={dogImageURL} alt='A Random Dog' />
+    </>
+  );
+}
+
+export default App;
